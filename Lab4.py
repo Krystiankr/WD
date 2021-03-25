dff4ccb906e7dfa094aed15dfc798bdcfb9a9f6c
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2021-03-25 18:43:09 +0100

    Update Lab4.py

--- Lab4.py
+++ Lab4.py
@@ -1,3 +1,30 @@
+##Zadanie 13
+from typing import Generator
+def prime_number() -> Generator[int, None, None]:
+    licznik = 0
+    tmp = 0
+    while True:
+        yield licznik
+        if (licznik == 0):
+            print(f"F{licznik}: {tmp}")
+            prev = 0
+        elif (licznik == 1):
+            print(f"F{licznik}: 1")
+
+            next = 1
+        else:
+
+            tmp = prev + next
+            prev = next
+            next = tmp
+            print(f"F{licznik}: {tmp}")
+
+
+        licznik += 1
+if __name__ == "__main__":
+    generator: Generator[int, None, None] = prime_number()
+    for x in range(27):
+        next(generator)
 ##Zadanie 14
 import math
 from typing import Generator
